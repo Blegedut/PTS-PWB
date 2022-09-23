d9ca6943ece6a0fed959dce27c0b0ffc057f73df
Blegedut <blegedut07@gmail.com>
2022-09-23 08:28:50 +0700

    Nomer3

---
+++ app/Http/Controllers/TarikTunaiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tarik_tunai;
+use Illuminate\Http\Request;
+
+class TarikTunaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tarik_tunai  $tarik_tunai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tarik_tunai $tarik_tunai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tarik_tunai  $tarik_tunai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tarik_tunai $tarik_tunai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tarik_tunai  $tarik_tunai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tarik_tunai $tarik_tunai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tarik_tunai  $tarik_tunai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tarik_tunai $tarik_tunai)
+    {
+        //
+    }
+}
